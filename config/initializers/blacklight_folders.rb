86ef933af7ebd43288977b31a67d71f485decf50
Alicia Cozine <alicia@curationexperts.com>
2014-11-25 09:43:56 -0600

    generated blacklight_folders

---
+++ config/initializers/blacklight_folders.rb
@@ -0,0 +1,11 @@
+# Remove bookmark from the navbar
+CatalogController.blacklight_config.navbar.partials.delete(:bookmark)
+# Remove bookmark from catalog#index
+CatalogController.blacklight_config.index.document_actions.delete(:bookmark)
+# Remove bookmark from catalog#show
+CatalogController.blacklight_config.show.document_actions.delete(:bookmark)
+
+
+CatalogController.add_index_tools_partial(:folder, partial: 'blacklight/folders/folder_control')
+CatalogController.add_show_tools_partial(:folder, partial: 'blacklight/folders/show/add_to_folder', if: Proc.new { |ctx| ctx.current_ability.can?(:create, Blacklight::Folders::Folder) } )
+CatalogController.add_nav_action(:folder, partial: 'blacklight/nav/folders', if: Proc.new { |ctx| ctx.current_ability.can?(:index, Blacklight::Folders::Folder)  } )
